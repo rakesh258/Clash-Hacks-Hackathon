abd40990b1046b087fe4cedd944b606fa5171fec
Deepak <raj.deeepak@gmail.com>
2017-03-25 22:17:42 +0530

    Added employer and employe registration activity, added in manifest

---
+++ app/src/main/java/com/dipakkr/github/clashhackproject/employer/EmployerPreProfileUpdate.java
@@ -0,0 +1,10 @@
+package com.dipakkr.github.clashhackproject.employer;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by deepak on 25-03-2017.
+ */
+
+public class EmployerPreProfileUpdate extends AppCompatActivity {
+}
