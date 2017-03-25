9c07f6b5c0bcf78a6783d4fbaddabb8589f3ce9e
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-03-26 03:59:09 +0530

    Add: functionality to add employer data to database

--- app/src/main/java/com/dipakkr/github/clashhackproject/employer/EmployerPreProfileUpdate.java
+++ app/src/main/java/com/dipakkr/github/clashhackproject/employer/EmployerPreProfileUpdate.java
@@ -6,8 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.dipakkr.github.clashhackproject.R;
+import com.dipakkr.github.clashhackproject.employer.model.Employer;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -18,6 +20,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class EmployerPreProfileUpdate extends AppCompatActivity {
 
+    private FirebaseDatabase mdatabase;
     private FirebaseAuth mAuth;
     private DatabaseReference mRef;
 
@@ -28,8 +31,9 @@ public class EmployerPreProfileUpdate extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employer_pre_update);
 
+        mdatabase = FirebaseDatabase.getInstance();
         mAuth = FirebaseAuth.getInstance();
-        mRef = FirebaseDatabase.getInstance().getReference();
+        mRef = mdatabase.getReference().child("employer");
 
         et_name = (EditText)findViewById(R.id.employer_name);
         et_intro = (EditText)findViewById(R.id.employer_intro);
@@ -47,5 +51,17 @@ public class EmployerPreProfileUpdate extends AppCompatActivity {
     }
     private void updateProfile(){
         //TODO add update profile code after making model
+        String employer_name = et_name.getText().toString();
+        String employer_intro = et_intro.getText().toString();
+        String employer_city = et_city.getText().toString();
+        String employer_mobile = et_mobile.getText().toString();
+        String employer_email = "";
+        String employer_skill = et_skill.getText().toString();
+
+        Employer employer = new Employer(employer_name,employer_intro,employer_city,employer_mobile,employer_email, employer_skill);
+        mRef.push().setValue(employer);
+
+        Toast.makeText(getApplicationContext(),"Database Updated", Toast.LENGTH_SHORT).show();
+
     }
 }
