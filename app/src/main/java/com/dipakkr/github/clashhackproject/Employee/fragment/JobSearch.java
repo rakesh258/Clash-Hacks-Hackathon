9e05bbd4770a3fcd24825d069e7215446bd1d6a2
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-03-26 13:35:24 +0530

    Update: Employer profile layout and employee job search fragment

--- app/src/main/java/com/dipakkr/github/clashhackproject/Employee/fragment/JobSearch.java
+++ app/src/main/java/com/dipakkr/github/clashhackproject/Employee/fragment/JobSearch.java
@@ -1,11 +1,24 @@
 package com.dipakkr.github.clashhackproject.Employee.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dipakkr.github.clashhackproject.R;
 
 /**
  * Created by deepak on 26-03-2017.
  */
 
 public class JobSearch extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        View view = inflater.inflate(R.layout.frag_employee_search, container, false);
+        return view;
+    }
 }
